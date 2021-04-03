91e61135ed7e5a40bc8dd17f8b763ab8064cc017
Hongbin Chen <hb@hongbins-mbp.lan>
2021-04-02 22:25:38 -0400

    path update

--- src/app/img-carousel/img-carousel.component.ts
+++ src/app/img-carousel/img-carousel.component.ts
@@ -16,17 +16,17 @@ export class ImgCarouselComponent implements OnInit {
     {
       crimeTitle: 'Xiao Zhen Xie, 75, was punched by a white man',
       crimeDescription: 'Video footage of the aftermath of the attack against Xiao Zhen Xie, 75, prompted outrage across the United States.Credit...CBS San Francisco',
-      img: '../../assets/img/XiaoZhenXie.png'
+      img: './assets/img/XiaoZhenXie.png'
     },
     {
       crimeTitle: 'Atlanta SPA Shootings',
       crimeDescription: 'Six Asian women dead including Emily Tan',
-      img: '../../assets/img/AtlantaShooting.png'
+      img: './assets/img/AtlantaShooting.png'
     },
     {
       crimeTitle: 'This man assault on a 65-year-old Asian woman',
       crimeDescription: 'Brandon Elliot was out on parole for killing his mother',
-      img: '../../assets/img/BrandonElliot.png'
+      img: './assets/img/BrandonElliot.png'
     }
   ]
 
